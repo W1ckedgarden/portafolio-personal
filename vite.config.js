9c0d4d1d3fe75ae7dbe14b046c3a827d10465479
Diego Cea <diego.cea10@inacapmail.cl>
2025-07-11 17:04:07 -0400

    Configuracion base URL para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'  // vite.config.js
+import react from '@vitejs/plugin-react'  // Importa el plugin de React para Vite
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig({  // Configuración de Vite para el proyecto
+  plugins: [react()],  // Añade el plugin de React
+  base: '/portafolio-personal/',  // Establece la base del proyecto para el despliegue en GitHub Pages
 })
